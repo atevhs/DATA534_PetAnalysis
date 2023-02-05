3842620211808a44d8979ffbfab81b2b64046b14
a-foote <alyssa.k.foote@gmail.com>
2023-02-04 18:00:45 -0800

    adding test_plot.py, updated TestSuite.py, updated ProjectNotebook-Alyssa.txt

---
+++ tests/testcases/test_plot.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import unittest as ut
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../petanalysis/")
+import plot as dogplt
+
+class test_plot(ut.TestCase):
+    @classmethod
+    def setup_class(cls):
+        print("Testing plot module:")
+    
+    def setUp(self):
+        print("setup unit test.")
+        self.dogs = pd.read_csv("../../petanalysis/Dog Data all.csv")
+    
+    def tearDown(self):
+        print("Teardown unit test.")
+    
+    def test_age_historgram(self):
+        with ut.mock.patch("plot.plt.show") as show_patch:
+            dogplt.age_histogram(self.dogs)
+            assert show_patch.called
+            
+    def test_size_heatmap(self):
+        with ut.mock.patch("plot.plt.show") as show_patch:
+            dogplt.size_heatmap(self.dogs)
+            assert show_patch.called
+
+    @classmethod
+    def tearDownClass(cls):
+        print('Plot testing completed.')
+
+ut.main(argv=[''], verbosity=2, exit=False)
