89cb651e30846fe3822cab8955b39a01689280b4
a-foote <alyssa.k.foote@gmail.com>
2023-02-09 16:24:57 -0800

    fixing build errors

--- petanalysis/location.py
+++ petanalysis/location.py
@@ -27,7 +27,7 @@ def get_shelter(postcode = None):
     """
     
     us =  pd.read_csv("petanalysis/US_postcode.csv", dtype = str)
-
+    
     if postcode == None:
         try:
             print('Enter your postcode:')
